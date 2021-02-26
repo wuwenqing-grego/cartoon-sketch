96f890b39a8b28712529b64ec9ebd9f7c05d7816
wutongxue <wuwenqingapple@icloud.com>
2021-02-26 23:45:28 +0800

    deploy

--- dist/preset.0b215970.js
+++ dist/preset.0b215970.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60452" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61402" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=cartoon-sketch/dist/preset.0b215970.js.map
+//# sourceMappingURL=preset.0b215970.js.map
